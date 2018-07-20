06ff2336829d5a61743ff9108545a056aea03006
ApostaC <jc4xvyp@outlook.com>
2018-07-20 19:25:06 +0800

    class Buffer

--- parser-hsrnet/src/Buffer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BUFFER_HH_
-#define _BUFFER_HH_
-
-
-
-#endif
---
+++ parser-hsrnet/src/Buffer.hpp
@@ -0,0 +1,14 @@
+#ifndef _BUFFER_HPP_
+#define _BUFFER_HPP_
+
+
+template<typename T>
+class Buffer
+{
+    public:
+    private:
+};
+
+
+
+#endif
